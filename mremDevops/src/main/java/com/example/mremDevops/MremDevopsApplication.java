01a096731f51a501184441e86b3c32da61f0ff48
scalargitid <pulkit.sharma.2406@gmail.com>
2025-10-16 14:45:33 +0530

    initial commit

---
+++ mremDevops/src/main/java/com/example/mremDevops/MremDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mremDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MremDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MremDevopsApplication.class, args);
+	}
+
+}
---
+++ mremDevops/src/test/java/com/example/mremDevops/MremDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mremDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MremDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
