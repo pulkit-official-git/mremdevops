02a15af550e9694930be16171b906f5e10ca5704
scalargitid <pulkit.sharma.2406@gmail.com>
2025-10-16 15:09:33 +0530

    second commit

--- mremDevops/src/main/java/com/example/mremDevops/DemoController.java
+++ mremDevops/src/main/java/com/example/mremDevops/DemoController.java
@@ -10,6 +10,6 @@ public class DemoController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello mrem";
+        return "Welcome to the devops session at mrem";
     }
 }
